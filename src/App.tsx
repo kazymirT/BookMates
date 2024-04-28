de8b1e7471c56d832e70598977d1b2a1168a79ff
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-04-28 09:43:57 +0300

    feat: create and configure repository

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.scss';
+const App = () => {
+  return (
+    <>
+      <h1>Online store</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ stylelint.config.mjs
@@ -0,0 +1,23 @@
+export default {
+  defaultSeverity: 'warning',
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-css-modules',
+    'stylelint-config-clean-order',
+    'stylelint-config-recommended-scss',
+  ],
+  overrides: [
+    {
+      files: ['**/*.module.scss'],
+      rules: {
+        'selector-class-pattern': [
+          '^[a-z]+([A-Z][a-zA-Z]+)*$',
+          {
+            message: (selector) =>
+              `Expected class selector "${selector}" to be lowerCamelCase`,
+          },
+        ],
+      },
+    },
+  ],
+};
