f9c4f82fed27bb63b7ce1a2df4ab4c6fc0445ebe
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-06-03 17:33:33 +0300

    feat: add Pagination component

--- src/Pages/Catalog/Catalog.tsx
+++ src/Pages/Catalog/Catalog.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import styles from './Catalog.module.scss';
 import Filter from './Filter/Filter';
 import BookCard from '@/components/BookCard/BookCard';
+import Pagination from '@/components/Pagination/Pagination';
 import Select from '@/components/ui-components/Select/Select';
 import { categories } from '@/utils/constants';
 import { catalogBooks } from '@/utils/fake';
@@ -14,12 +15,20 @@ const selectOptions = [
   'Спочатку нові',
   'За назвою',
 ];
+const totalElements = 35;
+const elementsPerPage = 9;
 
 const Catalog = () => {
   const [selectValue, setSelectValue] = useState<string>('За популярністю');
+  const [currentPage, setCurrentPage] = useState<number>(0);
   const handleChangeSelect = (value: string) => {
     if (selectValue !== value) {
       setSelectValue(value);
+    }
+  };
+  const handleChangePage = (nextPage: number) => {
+    if (currentPage !== nextPage) {
+      setCurrentPage(nextPage);
     }
   };
   return (
@@ -40,11 +49,19 @@ const Catalog = () => {
           <aside className={styles.filters}>
             <Filter title="Категорії" filters={categories} />
           </aside>
-          <section className={styles.books}>
-            {catalogBooks &&
-              catalogBooks.map((book) => (
-                <BookCard key={book.id} data={book} />
-              ))}
+          <section className={styles.box}>
+            <div className={styles.books}>
+              {catalogBooks &&
+                catalogBooks.map((book) => (
+                  <BookCard key={book.id} data={book} />
+                ))}
+            </div>
+            <Pagination
+              elementsPerPage={elementsPerPage}
+              totalElements={totalElements}
+              currentPage={currentPage}
+              onChange={handleChangePage}
+            />
           </section>
         </div>
       </section>
