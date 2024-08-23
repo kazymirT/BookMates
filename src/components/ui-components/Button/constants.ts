cc9c2625e21b56055e992881564c9f64447c1510
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-08-23 12:52:21 +0300

    feat: add delete and accent variants to Button component

--- src/components/ui-components/Button/constants.ts
+++ src/components/ui-components/Button/constants.ts
@@ -4,6 +4,7 @@ export enum Sizes {
   Medium = 'm',
   Large = 'l',
   ExtraLarge = 'extra_l',
+  FullS = 'full-s',
   Full = 'full',
 }
 
@@ -11,7 +12,9 @@ export enum Variant {
   Basic = 'basic',
   Primary = 'primary',
   Label = 'label',
+  Delete = 'delete',
   LabelX = 'label-x',
+  Accent = 'accent',
 }
 
 export enum ButtonType {
