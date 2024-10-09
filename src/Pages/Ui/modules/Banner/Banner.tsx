51d408aff1fe661671df69d9352688f3d3a56753
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-10-09 12:57:29 +0300

    fix: correct issues in Banner component

--- src/Pages/Ui/modules/Banner/Banner.tsx
+++ src/Pages/Ui/modules/Banner/Banner.tsx
@@ -1,17 +1,28 @@
+import { useRef } from 'react';
+import SwiperCore from 'swiper';
 import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import styles from './Banner.module.scss';
 import 'swiper/css';
-import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { BANNER_DATA } from './data';
 import Slide from './Slide/Slide';
+import { SliderButton } from '../../components/SliderButton/SliderButton';
 
 const Banner = () => {
+  const swiperRef = useRef<SwiperCore | null>(null);
+  const handleNext = () => {
+    swiperRef.current?.slideNext();
+  };
+
+  const handlePrev = () => {
+    swiperRef.current?.slidePrev();
+  };
   return (
     <div className={styles.banner}>
       <Swiper
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
         className={styles.swiper}
         pagination={{
           clickable: true,
@@ -34,6 +45,11 @@ const Banner = () => {
               <Slide slide={banner} />
             </SwiperSlide>
           ))}
+        <SliderButton
+          onNext={handleNext}
+          onPrev={handlePrev}
+          variant="banner"
+        />
       </Swiper>
     </div>
   );
