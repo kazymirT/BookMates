b7f38a83cdedd5dfecb696018888b7f9728b89ea
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-10-30 17:38:50 +0200

    refactor: update logic and styling for Filter component

--- src/Pages/Catalog/Filters/FilterContent.tsx
+++ src/Pages/Catalog/Filters/FilterContent.tsx
@@ -2,14 +2,15 @@ import { FC, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import Filter from './Filter/Filter';
+import styles from './FilterContent.module.scss';
 import PriceFilter from './PriceFilter/PriceFilter';
+import Sort from './Sort/Sort';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { type AllAttributes } from '@/redux/services/services.types';
-import { initializeState } from '@/redux/slices/queryParams';
+import { clearFilters, initializeState } from '@/redux/slices/queryParams';
 import { queryAllData } from '@/redux/slices/queryParams';
 import { initializeQueryState } from '@/utils/initializeQueryState';
 import { updateFilterParams } from '@/utils/updateFilterParams';
-
 interface FilterContentProps {
   attributes: AllAttributes;
 }
@@ -18,7 +19,7 @@ const FilterContent: FC<FilterContentProps> = ({ attributes }) => {
   const { t } = useTranslation();
 
   const dispatch = useAppDispatch();
-
+  const clearFilter = () => dispatch(clearFilters());
   useEffect(() => {
     const newState = initializeQueryState(attributes);
     dispatch(initializeState(newState));
@@ -26,38 +27,55 @@ const FilterContent: FC<FilterContentProps> = ({ attributes }) => {
 
   const { filter, price: priceChecked } = useAppSelector(queryAllData);
 
-  const { categories, language, price, years } = updateFilterParams(
+  const { language, price, years } = updateFilterParams(
     filter,
     priceChecked,
     attributes
   );
   return (
-    <>
-      <Filter
+    <div className={styles.content}>
+      <div className={styles.filters}>
+        {/* <Filter
         title={t('catalog.filter.categories')}
         categories={categories}
         filterType="categories"
         isDefaultOpen
-      />
-      <Filter
-        title={t('catalog.filter.language')}
-        filterType="language"
-        categories={language}
-        isDefaultOpen
-      />
-      <Filter
-        title={t('catalog.filter.years')}
-        filterType="years"
-        categories={years.sort((a, b) => b.id - a.id)}
-        isScroll
-        isDefaultOpen
-      />
-      <PriceFilter
-        title={t('catalog.filter.price')}
-        isDefaultOpen
-        price={price}
-      />
-    </>
+      /> */}
+        <Filter
+          title={
+            language.count !== 0
+              ? t('catalog.filter.languageWithCount', { count: language.count })
+              : t('catalog.filter.language')
+          }
+          filterType="language"
+          categories={language.items}
+          isDefaultOpen
+        />
+        <Filter
+          title={
+            years.count !== 0
+              ? t('catalog.filter.yearsWithCount', { count: years.count })
+              : t('catalog.filter.years')
+          }
+          filterType="years"
+          categories={years.items.sort((a, b) => b.id - a.id)}
+          isScroll
+          isDefaultOpen
+        />
+        <PriceFilter
+          title={t('catalog.filter.price', {
+            price1: price[0],
+            price2: price[1],
+          })}
+          isDefaultOpen
+          price={price}
+        />
+        <Sort />
+      </div>
+      <button type="button" onClick={clearFilter} className={styles.clear}>
+        {t('catalog.clear-filter')}
+      </button>
+    </div>
   );
 };
 
