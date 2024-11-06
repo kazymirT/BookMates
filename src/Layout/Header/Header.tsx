0c58857849d6b8a680d688c07f6d3587eb6763c4
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-11-06 11:06:46 +0200

    feat: transform header to sticky header for enhanced UX

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -5,7 +5,7 @@ import styles from './Header.module.scss';
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.container}>
+      <div className={'container'}>
         <div className={styles['header-inner']}>
           <HeaderTop />
           <Bottom />
