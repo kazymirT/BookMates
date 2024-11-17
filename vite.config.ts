5a1553e99331f4b9766df14e4655f9b245781c5c
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-11-17 13:26:51 +0200

    chore: update Vite config to include source maps in build

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: true, // Додає карти джерел для JavaScript під час білду
+    sourcemap: true,
   },
   resolve: {
     alias: {
