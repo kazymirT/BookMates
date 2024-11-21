95f69b31ccf5adf8b5885151dd8cc20ea65be3b4
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-11-21 17:31:06 +0200

    refactor: extract logic from ProductControl into useProductControlLogic hook

--- src/Pages/Product/ProductControl/ProductControl.tsx
+++ src/Pages/Product/ProductControl/ProductControl.tsx
@@ -1,3 +1,4 @@
+import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import styles from '../Product.module.scss';
@@ -5,92 +6,67 @@ import Price from '@/components/Price/Price';
 import { Button } from '@/components/ui-components/Button/Button';
 import {
   ButtonType,
+  Position,
   Sizes,
   Variant,
-  Position,
 } from '@/components/ui-components/Button/constants';
 import { ButtonLink } from '@/components/ui-components/ButtonLink/ButtonLink';
 import { Icon } from '@/components/ui-components/Icons';
-import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import { useProductControlLogic } from '@/hooks/useProductControlLogic';
 import { BookById } from '@/redux/services/services.types';
-import { toggleShowCartNotification } from '@/redux/slices/cartNotificationSlice';
-//import { useAddBookMutation } from '@/redux/services/cart';
-import {
-  addGoods,
-  goods,
-  toggleOpenCart,
-} from '@/redux/slices/shoppingCartSlice';
 
-const ProductControl = ({ book }: { book: BookById }) => {
+interface ProductControlProps {
+  book: BookById;
+}
+
+const ProductControl: FC<ProductControlProps> = ({ book }) => {
   const { t } = useTranslation();
+  const { isBookInCart, handleAddToCart, handleOpenCart } =
+    useProductControlLogic(book);
 
-  const dispatch = useAppDispatch();
-  const goobs = useAppSelector(goods);
-  const isBookInCard = goobs.some((goob) => goob.id === book?.id);
-  //const [ addToCart ] = useAddBookMutation()
-  const handleAddToCart = () => {
-    //addToCart({bookId: 1, quantity: 1})
-    if (book && !isBookInCard) {
-      dispatch(
-        addGoods({
-          id: book.id,
-          price: String(book.price),
-          authors: book.authors.map((author) => author.name),
-          img: book.imageUrl,
-          title: book.title,
-          discount: book.discount,
-          discountPrice: book.discountPrice,
-        })
-      );
-      dispatch(toggleShowCartNotification(true));
-    }
-  };
-  const handleOpenCart = () => dispatch(toggleOpenCart(true));
   return (
     <div className={styles.control}>
-      <>
-        <div className={styles.price}>
-          <Price
-            normalPrice={book.price}
-            variant="product"
-            discountPrice={book.discount ? book.discountPrice : undefined}
-          />
-          <span className={styles.is}>Товар у наявності</span>
-        </div>
-        <div className={styles.btns}>
-          <ButtonLink
-            url="/order"
+      <div className={styles.price}>
+        <Price
+          normalPrice={book.price}
+          variant="product"
+          discountPrice={book.discount ? book.discountPrice : undefined}
+        />
+        <span className={styles.is}>{t('product.available')}</span>
+      </div>
+      <div className={styles.btns}>
+        <ButtonLink
+          url="/order"
+          buttonType={ButtonType.Button}
+          size={Sizes.FullM}
+          text={t('product.btn-buy')}
+          variant={Variant.Basic}
+          onClick={handleAddToCart}
+        />
+        {!isBookInCart ? (
+          <Button
             buttonType={ButtonType.Button}
+            text={t('product.btn-basket')}
+            variant={Variant.Primary}
+            iconPosition={Position.Left}
             size={Sizes.FullM}
-            text={t('product.btn-buy')}
-            variant={Variant.Basic}
             onClick={handleAddToCart}
+            icon={<Icon.Cart />}
           />
-          {!isBookInCard ? (
+        ) : (
+          <div className={styles['product_in_card']}>
             <Button
               buttonType={ButtonType.Button}
-              text={t('product.btn-basket')}
-              variant={Variant.Primary}
+              text={t('product.btn-in-basket')}
+              variant={Variant.Basic}
               iconPosition={Position.Left}
               size={Sizes.FullM}
-              onClick={handleAddToCart}
-              icon={<Icon.Cart />}
+              onClick={handleOpenCart}
+              icon={<Icon.Arrow_1 className={styles.arrow} />}
             />
-          ) : (
-            <div className={styles['product_in_card']}>
-              <Button
-                buttonType={ButtonType.Button}
-                text="У кошику"
-                variant={Variant.Basic}
-                iconPosition={Position.Left}
-                size={Sizes.FullM}
-                onClick={handleOpenCart}
-                icon={<Icon.Arrow_1 className={styles.arrow} />}
-              />
-            </div>
-          )}
-        </div>
-      </>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
