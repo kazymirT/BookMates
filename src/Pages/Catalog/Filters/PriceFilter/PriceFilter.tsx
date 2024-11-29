96b34265652eb88d8c10d697946e7872f9a48fd9
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-11-29 13:47:18 +0200

    refactor: integrate updated DropDown component into Catalog page

--- src/Pages/Catalog/Filters/PriceFilter/PriceFilter.tsx
+++ src/Pages/Catalog/Filters/PriceFilter/PriceFilter.tsx
@@ -4,11 +4,10 @@ import ReactSlider from 'react-slider';
 
 import styles from './PriceFilter.module.scss';
 import { type PriceFilterProps } from '../../Catalog.types';
-import DropDown from '@/components/ui-components/Dropdown/DropDown';
 import { useAppDispatch } from '@/redux/hooks';
 import { setPrice } from '@/redux/slices/queryParams';
 
-const PriceFilter = ({ title, price }: PriceFilterProps) => {
+const PriceFilter = ({ price }: PriceFilterProps) => {
   const dispatch = useAppDispatch();
   const [value, setValue] = useState<number[]>(price);
   const [isError, setIsError] = useState(false);
@@ -32,55 +31,44 @@ const PriceFilter = ({ title, price }: PriceFilterProps) => {
   const onSubmit = () => {
     dispatch(setPrice([String(value[0]), String(value[1])]));
   };
-  const controlClassNames = classNames(styles.control, {
-    [styles.open]: true,
-  });
+
   const inputClassNames = classNames(styles.input, {
     [styles['input__error']]: isError,
   });
   return (
-    <div className={styles.filter}>
-      <DropDown
-        tagName="BUTTON"
-        variant="filter"
-        control={<h3 className={styles.title}>{title}</h3>}
-        options={
-          <div className={controlClassNames}>
-            <ReactSlider
-              value={value[1] >= value[0] ? value : undefined}
-              max={10000}
-              min={0}
-              minDistance={0}
-              onChange={(value) => {
-                setValue(value);
-                setIsError(false);
-              }}
-              className={styles['range-slider']}
-              thumbClassName={styles['range-slider__thumb']}
-            />
-            <div className={styles['price-control']} onSubmit={onSubmit}>
-              <div className={styles.inputs}>
-                <input
-                  className={inputClassNames}
-                  type="text"
-                  value={value[0]}
-                  onChange={handlerLowerBound}
-                />
-                <span></span>
-                <input
-                  className={inputClassNames}
-                  type="text"
-                  value={value[1]}
-                  onChange={handlerUpperBound}
-                />
-              </div>
-              <button type="button" onClick={onSubmit} disabled={isError}>
-                ok
-              </button>
-            </div>
-          </div>
-        }
+    <div className={styles.control}>
+      <ReactSlider
+        value={value[1] >= value[0] ? value : undefined}
+        max={10000}
+        min={0}
+        minDistance={0}
+        onChange={(value) => {
+          setValue(value);
+          setIsError(false);
+        }}
+        className={styles['range-slider']}
+        thumbClassName={styles['range-slider__thumb']}
       />
+      <div className={styles['price-control']} onSubmit={onSubmit}>
+        <div className={styles.inputs}>
+          <input
+            className={inputClassNames}
+            type="text"
+            value={value[0]}
+            onChange={handlerLowerBound}
+          />
+          <span></span>
+          <input
+            className={inputClassNames}
+            type="text"
+            value={value[1]}
+            onChange={handlerUpperBound}
+          />
+        </div>
+        <button type="button" onClick={onSubmit} disabled={isError}>
+          ok
+        </button>
+      </div>
     </div>
   );
 };
