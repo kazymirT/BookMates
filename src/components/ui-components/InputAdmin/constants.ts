f245f1fba482118351a124aeeaa967e015806ae7
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-12-10 19:54:32 +0200

    test: add unit tests for InputAdmin component

---
+++ src/components/ui-components/InputAdmin/constants.ts
@@ -0,0 +1,5 @@
+export const INPUT_ADMIN_CONTAINER = 'input-admin-container';
+export const INPUT_ADMIN_INPUT = 'input-admin-input';
+export const INPUT_ADMIN_LABEL = 'input-admin-label';
+export const INPUT_ADMIN_ERROR = 'input-admin-error';
+export const INPUT_ADMIN_PHONE = 'input-admin-phone';
