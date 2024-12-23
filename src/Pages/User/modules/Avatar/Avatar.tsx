502c3c0e9c2444183d38aebed41b25a96b7c5edf
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-12-23 20:17:50 +0200

    feat: add Avatar component

--- src/Pages/User/modules/Avatar/Avatar.tsx
+++ src/Pages/User/modules/Avatar/Avatar.tsx
@@ -1,14 +1,7 @@
 import styles from './Avatar.module.scss';
-import { Icon } from '@/components/ui-components/Icons';
 
 const Avatar = () => {
-  return (
-    <div className={styles.avatar}>
-      <div className={styles.icon}>
-        <Icon.Account width="55" height="55" />
-      </div>
-    </div>
-  );
+  return <div className={styles.avatar}> </div>;
 };
 
 export default Avatar;
