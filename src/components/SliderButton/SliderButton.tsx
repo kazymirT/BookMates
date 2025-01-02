02adb8a3c2375d779aa00b5c4b2bda1313d8cd84
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-01-02 19:13:54 +0200

    feat: add SliderButton component

---
+++ src/components/SliderButton/SliderButton.tsx
@@ -0,0 +1,27 @@
+import classNames from 'classnames';
+import { FC } from 'react';
+
+import styles from './SliderButton.module.scss';
+import { SliderButtonProps } from './types';
+import { Icon } from '@/components/ui-components/Icons';
+
+export const SliderButton: FC<SliderButtonProps> = ({
+  variant,
+  arrow,
+  onClick,
+}) => {
+  const btnNextClassName = classNames(styles.btn, {
+    [styles[`btn__${arrow}__${variant}`]]: variant,
+    [styles[`btn__${arrow}`]]: arrow,
+  });
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      aria-label={`slide ${arrow}`}
+      className={btnNextClassName}
+    >
+      <Icon.Arrow_1 />
+    </button>
+  );
+};
