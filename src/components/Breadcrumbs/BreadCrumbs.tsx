1b07ea58d94b6ae231ed07dfa1d126cc0f80cd5e
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-01-14 13:10:06 +0200

    feat: add Skeleton for BreadCrumbs component

--- src/components/Breadcrumbs/BreadCrumbs.tsx
+++ src/components/Breadcrumbs/BreadCrumbs.tsx
@@ -4,36 +4,50 @@ import { Link } from 'react-router-dom';
 
 import styles from './Breadcrumbs.module.scss';
 import { BreadCrumbsProps } from './types';
-import { useAppDispatch } from '@/redux/hooks';
+import Skeleton from '../Skeleton/components/Skeleton/Skeleton';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { clearFilters } from '@/redux/slices/queryParams';
+import { isLoading } from '@/redux/slices/skeletonSlice';
 
 const Breadcrumbs: FC<BreadCrumbsProps> = ({
   options,
   activeLastLink = false,
 }) => {
+  const isSkeleton = useAppSelector(isLoading);
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
   const clearFilter = () => dispatch(clearFilters());
 
   return (
     <ul className={styles.breadcrumbs}>
-      <li>
-        <Link to={'/'}>{t('breadcrumbs.home')}</Link>
-      </li>
-      {options.map(({ name, to }, index) => (
-        <li key={index}>
-          <span className={styles.separation}>&#8594;</span>
-          {index !== options.length - 1 ? (
-            <Link to={to} onClick={to === '/catalog' ? clearFilter : undefined}>
-              {name}
-            </Link>
-          ) : activeLastLink ? (
-            <Link to={to}>{name}</Link>
-          ) : (
-            <span>{name}</span>
-          )}
+      {isSkeleton ? (
+        <li>
+          <Skeleton variant="title" />
         </li>
-      ))}
+      ) : (
+        <>
+          <li>
+            <Link to={'/'}>{t('breadcrumbs.home')}</Link>
+          </li>
+          {options.map(({ name, to }, index) => (
+            <li key={index}>
+              <span className={styles.separation}>&#8594;</span>
+              {index !== options.length - 1 ? (
+                <Link
+                  to={to}
+                  onClick={to === '/catalog' ? clearFilter : undefined}
+                >
+                  {name}
+                </Link>
+              ) : activeLastLink ? (
+                <Link to={to}>{name}</Link>
+              ) : (
+                <span>{name}</span>
+              )}
+            </li>
+          ))}
+        </>
+      )}
     </ul>
   );
 };
