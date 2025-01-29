8e96dc16469ad6918e4a1d6dcdea2fa95464a3b5
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-01-29 17:19:19 +0200

    refactor: update Footer component

--- src/Layout/Footer/Footer.tsx
+++ src/Layout/Footer/Footer.tsx
@@ -1,51 +1,13 @@
-import { useTranslation } from 'react-i18next';
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
-
 import styles from './Footer.module.scss';
-import appstore from '@/assets/icons/AppStore.svg';
-import googleplay from '@/assets/icons/GooglePlay.svg';
-import { toggleModal } from '@/redux/slices/modalSlice';
+import Copyright from './modules/Copyright/Copyright';
+import Navigation from './modules/Navigation/Navigation';
 
 const Footer = () => {
-  const { t } = useTranslation();
-  const dispatch = useDispatch();
-  const handleOnSupport = () =>
-    dispatch(toggleModal({ openedModalType: 'feedback' }));
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <nav>
-          <ul className={styles.links}>
-            <li>
-              <Link to={'/'}>{t('footer.links.main')}</Link>
-            </li>
-            <li>
-              <Link to={'/delivery'}>{t('footer.links.delivery')}</Link>
-            </li>
-            <li onClick={handleOnSupport}>{t('footer.links.support')}</li>
-          </ul>
-          <div className={styles.btns}>
-            <img
-              src={appstore}
-              alt="app store"
-              width={160}
-              height={48}
-              loading="lazy"
-            />
-            <img
-              src={googleplay}
-              alt="google play"
-              width={160}
-              height={48}
-              loading="lazy"
-            />
-          </div>
-        </nav>
-        <div>
-          <span>&copy;</span>
-          <span>{t('footer.copyright')}</span>
-        </div>
+        <Navigation />
+        <Copyright />
       </div>
     </footer>
   );
