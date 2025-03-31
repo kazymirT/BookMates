db691600e61495c76ec7d4c4bc624c9724001b60
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-03-31 13:04:49 +0300

    feat: add web-vitals

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 import './styles/index.scss';
+import reportWebVitals from './reportWebVitals.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -11,3 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <SpeedInsights />
   </React.StrictMode>
 );
+
+// eslint-disable-next-line no-console
+reportWebVitals(console.log);
